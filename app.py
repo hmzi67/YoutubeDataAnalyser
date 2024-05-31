07be527dc88ac91a2b77cd68f6318e9145c3e518
Muhammad Naveed <muhammadnaveedcis@gmail.com>
2024-05-31 16:54:14 +0500

    cleaning code

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 from src import youtube_data_module as ydt
 from src import viz
-import pandas as pd
 import os
 import logging
 import sys
@@ -21,7 +20,7 @@ def home():
 
 @app.route('/select_video')
 def select_video():
-    '''This page returns search results, when a user hits the 'Search Video' button'''
+    # This page returns search results, when a user hits the 'Search Video' button
     result_dictionary = request.args
     query = result_dictionary['query']
     youtube = ydt.youtubeAPIkey(API_KEY)
@@ -35,7 +34,7 @@ def select_video():
 
 @app.route('/video_comments')
 def video_comments():
-    '''This page returns a video comment analysis, when a user hits the 'See video comment analysis' button'''
+    # This page returns a video comment analysis, when a user hits the 'See video comment analysis' button
     video_id = request.args.get('video_id')
 
     youtube = ydt.youtubeAPIkey(API_KEY)
@@ -56,75 +55,11 @@ def video_comments():
     image_names.append(viz.lineplot_cumsum_video_comments(comment_sentiment2, video_id))
     image_names.append(viz.lineplot_cumsum_video_comments_pos_neg(comment_sentiment2, pos_sent, neg_sent, video_id))
     image_names.append(viz.scatterplot_sentiment_likecount(comment_sentiment2, pos_sent, neg_sent, video_id))
-    # Calculate correlation
-    # like_count_sentiment_corr = round(comment_sentiment2.corr().loc['like_count'][5],2)
 
     return render_template(
         'video_comments.html',
         image_names=image_names,
-        # like_count_sentiment_corr=like_count_sentiment_corr
     )
-
-# @app.route('/select_channels', methods=['GET', 'POST'])
-# def select_channels():
-#     '''This page return search results for the channel queries a user inputs and hits the 'Search Channels' button'''
-#     result_dictionary = request.args
-#     channel_names = []
-
-#     for channel_name in result_dictionary:
-#         if len(result_dictionary.get(channel_name)) > 0:
-#             channel_names.append(result_dictionary[channel_name])
-
-#     youtube = ydt.youtubeAPIkey(API_KEY)
-#     query_results = {}
-
-#     for cn in channel_names:
-#         result = ydt.youtubeSearchList(youtube, channel_id=None, q=cn, maxResults=5, type='channel')
-#         query_results[cn] =  result
-
-#     return render_template(
-#         'select_channels.html',
-#         query_results=query_results
-#     )
-
-# @app.route('/channels', methods=['GET', 'POST'])
-# def channels():
-#     '''This page returns the channel coparison analysis when a user selects at least one channel with a radio button and hits "Compare channels now"'''
-#     result_dictionary = request.args
-
-#     channel_ids = []
-#     for c_id in result_dictionary:
-#         if len(result_dictionary[c_id]) == 24:
-#             channel_ids.append(result_dictionary[c_id])
-
-#     youtube = ydt.youtubeAPIkey(API_KEY)
-#     video_df = ydt.get_channel_video_df(youtube, channel_ids)
-
-#     image_names = []
-#     image_names.append(viz.barplot_channel_video_count(video_df, channel_ids))
-#     image_names.append(viz.barplot_links(video_df, channel_ids))
-
-#     channel_titles = []
-#     for channel_id in channel_ids:
-#         channel_video_df = video_df[video_df['channel_id'] == channel_id]
-#         channel_title = channel_video_df['channel_title'].unique()[0]
-#         channel_titles.append(channel_title)
-#         image_names.append(viz.histogram_video_duration_count_single(channel_video_df, channel_id, channel_title=channel_title))
-#         channel_video_series = channel_video_df['tags']
-#         wordcloud_string = ydt.concat_listelements(channel_video_series)
-#         image_names.append(viz.create_wordcloud(wordcloud_string, stopwords=None, video_id=channel_id, channel_title=channel_title))
-
-#     df_table = viz.top_videos(video_df, metric='view', n=5)
-
-#     return render_template(
-#         'channels.html',
-#         result_dictionary=result_dictionary,
-#         video_df=video_df,
-#         image_names=image_names,
-#         channel_ids=channel_ids,
-#         channel_titles=channel_titles,
-#         tables=[df_table.to_html(index=False, classes='table-striped')],
-#     )
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
--- src/youtube_data_module.py
+++ src/youtube_data_module.py
@@ -1,6 +1,3 @@
-# YouTube Data Module Version 17
-# -*- coding: utf-8 -*-
-
 import os
 import googleapiclient.discovery
 import google_auth_oauthlib.flow
@@ -20,7 +17,7 @@ logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
 def video_categories(youtube, regionCode="None", part=None, id=None):
-    '''Return a json file of categories and a dict, that is reduced to ids and titles'''
+    # Return a json file of categories and a dict, that is reduced to ids and titles
 
     video_category_dict = {'1': 'Film & Animation', '2': 'Autos & Vehicles', '10': 'Music', '15': 'Pets & Animals', '17': 'Sports', '18': 'Short Movies', '19': 'Travel & Events', '20': 'Gaming', '21': 'Videoblogging', '22': 'People & Blogs', '23': 'Comedy', '24': 'Entertainment', '25': 'News & Politics', '26': 'Howto & Style', '27': 'Education', '28': 'Science & Technology', '29': 'Nonprofits & Activism', '30': 'Movies', '31': 'Anime/Animation', '32': 'Action/Adventure', '33': 'Classics', '34': 'Comedy', '35': 'Documentary', '36': 'Drama', '37': 'Family', '38': 'Foreign', '39': 'Horror', '40': 'Sci-Fi/Fantasy', '41': 'Thriller', '42': 'Shorts', '43': 'Shows', '44': 'Trailers'}
 
@@ -37,9 +34,7 @@ def video_categories(youtube, regionCode="None", part=None, id=None):
         return video_categories_response
 
 def youtubeAPIkey(DEVELOPER_KEY, OAUTHLIB_INSECURE_TRANSPORT = "1", api_service_name = "youtube", api_version = "v3"):
-    '''Get YouTube Data API credentials via API Key\n
-    Disable OAuthlib's HTTPS verification when running locally.\n
-    *DO NOT* leave this option enabled in production.'''
+    # Get YouTube Data API credentials via API Key. Disable OAuthlib's HTTPS verification when running locally. *DO NOT* leave this option enabled in production.
 
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = OAUTHLIB_INSECURE_TRANSPORT
     youtube = googleapiclient.discovery.build(
@@ -47,9 +42,7 @@ def youtubeAPIkey(DEVELOPER_KEY, OAUTHLIB_INSECURE_TRANSPORT = "1", api_service_
     return youtube
 
 def youtubeSearchList(youtube, channel_id=None, q=None, maxResults=50, type=None):
-    '''
-    Return a list of video snippets. \n Documentation: https://developers.google.com/youtube/v3/docs/search/list
-    '''
+    # Return a list of video snippets. \n Documentation: https://developers.google.com/youtube/v3/docs/search/list
     request = youtube.search().list(
         part="snippet"
         ,channelId=channel_id
@@ -62,16 +55,13 @@ def youtubeSearchList(youtube, channel_id=None, q=None, maxResults=50, type=None
     return responseSearchList
 
 def youtubeSearchListStatistics(youtube, q=None, maxResults=10):
-    '''Get video search results for a query. Returns advanced statistics such as counts forlikes, dislikes, views and comments. Return a json file.'''
+    # Get video search results for a query. Returns advanced statistics such as counts forlikes, dislikes, views and comments. Return a json file.
 
     query_result = youtubeSearchList(youtube, q=q, maxResults=maxResults, type='video')
 
-    # Create a list of video ids
     video_id_list = [x['id']['videoId'] for x in query_result['items']]
-
     snippets = video_snippets(youtube, video_id_list, maxResults=10, part="statistics")
     assert len(query_result['items']) == len(snippets), 'Query result length does not match statistics length.'
-
     counter = 0
     for i in query_result['items']:
         i['statistics'] = snippets[counter]['statistics']
@@ -80,9 +70,7 @@ def youtubeSearchListStatistics(youtube, q=None, maxResults=10):
     return query_result
 
 def videoIdList(youtube, channelId):
-    '''
-    Return a list of all public video ids (in a specific channel)
-    '''
+    # Return a list of all public video ids (in a specific channel)
     videoIdList = []
     requestChannelsList = youtube.channels().list(
         part="contentDetails"
@@ -91,10 +79,8 @@ def videoIdList(youtube, channelId):
     )
     responseChannelsList = requestChannelsList.execute()
 
-    # Get upload playlist id from dictionary
     channelUploadPlaylistID = responseChannelsList.get('items')[0].get('contentDetails').get('relatedPlaylists').get('uploads')
 
-    # Get items from upload playlist
     playlistNextPageToken = ''
 
     while playlistNextPageToken != None:
@@ -117,8 +103,7 @@ def videoIdList(youtube, channelId):
     return videoIdList
 
 def list_slice(input_list, n=50):
-    '''Concatenate n list elements and return a new list of concatenations.\n
-    Takes a list as input_list and an integer n for elements to concatenate.'''
+    # Concatenate n list elements and return a new list of concatenations. Takes a list as input_list and an integer n for elements to concatenate.
     s = 0
     e = n
     list_slices = []
@@ -131,17 +116,7 @@ def list_slice(input_list, n=50):
     return list_slices
 
 def videoSnippet(youtube, videoId, maxResults=50, part="snippet,statistics,contentDetails,player,status"):
-    '''
-    Return a infos of a specific video\n
-    Quota costs per video and info:\n
-    snippet 2
-    statistics 2
-    contentDetails 2
-    player 0
-    recordingDetails ?
-    status 2
-    topicDetails 2
-    '''
+    
     requestSnippet = youtube.videos().list(
         part=part
         ,id=videoId
@@ -150,17 +125,6 @@ def videoSnippet(youtube, videoId, maxResults=50, part="snippet,statistics,conte
     return responseSnippet
 
 def video_snippets(youtube, video_id_list, maxResults=50, part="snippet,statistics,contentDetails,player,status"):
-    '''
-    Return a infos of a single or several videos. Input is a list object of video ids.\n
-    Quota costs per video and info:\n
-    snippet 2
-    statistics 2
-    contentDetails 2
-    player 0
-    recordingDetails ?
-    status 2
-    topicDetails 2
-    '''
     video_id_chunks = list_slice(video_id_list, n=50)
 
     video_snippets =[]
@@ -172,7 +136,7 @@ def video_snippets(youtube, video_id_list, maxResults=50, part="snippet,statisti
 
 
 def youtubeOauth(scopes, api_service_name, api_version, client_secrets_file, OAUTHLIB_INSECURE_TRANSPORT):
-    '''Disable OAuthlib's HTTPS verification when running locally. *DO NOT* leave this option enabled in production.'''
+    # Disable OAuthlib's HTTPS verification when running locally. *DO NOT* leave this option enabled in production.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = OAUTHLIB_INSECURE_TRANSPORT
     flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
         client_secrets_file, scopes)
@@ -182,27 +146,26 @@ def youtubeOauth(scopes, api_service_name, api_version, client_secrets_file, OAU
     return youtube_analytics
 
 def csv_videolist(filename):
-    '''Open csv file and return content as object'''
+    # '''Open csv file and return content as object'''
     with open(filename, 'r') as wbs:
         content = csv.reader(wbs)
     return content
 
 def to_int(string):
-    '''Turn duration from text string such as 'PT1H23M09S' to an int'''
+    # '''Turn duration from text string such as 'PT1H23M09S' to an int'''
     return int(string[:-1]) if string else 0
 
 def get_duration_sec(pt):
-    '''Turn duration from text string such as 'PT1H23M09S' to an int of seconds'''
+    # '''Turn duration from text string such as 'PT1H23M09S' to an int of seconds'''
     pattern = 'PT(\d*H)?(\d*M)?(\d*S)?'
     timestamp = [to_int(x) for x in re.findall(pattern, pt)[0]]
     duration_sec = timestamp[0] * 3600 + timestamp[1] * 60 + timestamp[2]
     return duration_sec
 
 def snippets_to_dict(video_snippet_list, yt_credentials=None):
-    '''Return a dictionary from a given list of one or more video snippets.\
-    The dictionary is optimized for creating a dataframe'''
+    # '''Return a dictionary from a given list of one or more video snippets.\
+    # The dictionary is optimized for creating a dataframe'''
 
-    # Create an empty dictionary
     df_data = {'video_id': [],
                'published_at': [],
                'channel_id': [],
@@ -276,18 +239,6 @@ def snippets_to_dict(video_snippet_list, yt_credentials=None):
 
     return df_data
 
-# def get_channel_snippet(youtube, channel_id, nextPageToken=None):
-#     '''Get a cannel snippet. Take as input a string of one channel_id or a concatenated string of ids separated by commas without spaces.'''
-#     request = youtube.channels().list(
-#         part="snippet",
-#         id=channel_id,
-#         maxResults=50,
-#         pageToken=nextPageToken
-#     )
-#     response = request.execute()
-#     return response
-
-
 def get_comment_threads(
     youtube,
     part="id,replies,snippet",
@@ -296,16 +247,9 @@ def get_comment_threads(
     video_id=None,
     maxResults=100
 ):
-    '''Return a .json with top-level comments and meta data. Take as input the youtube credential object and the videoId.\n
-    Specify exactly one filter out of: channel_id, comment_thread_id, video_id.\n
-    See detailied info in the documentation: https://developers.google.com/youtube/v3/docs/commentThreads/list \n
-    Quota costs: id: 0, replies: 2, snippet: 2'''
-
-    # Get items from upload playlist
     commentThreadsNextPageToken = ''
     output = []
 
-    # Calculate the costs for query
     costs = 0
     cost_per_query = 0
     if 'replies' in part:
@@ -334,7 +278,7 @@ def get_comment_threads(
 
 
 def comment_threads_to_dict(comments):
-    '''Return a dictionary. Take as input a .json file with toplevel comments from threads'''
+    # '''Return a dictionary. Take as input a .json file with toplevel comments from threads'''
     df_data = {
     'comment_id':[],
     'author_display_name':[],
@@ -397,26 +341,16 @@ def comment_threads_to_dict(comments):
 
 
 def get_comments_list(youtube, part="id", maxResults=100, parent_id=None, id=None):
-    '''Return a list of ids and/or snippets for a given comment id. Specify exactly one filter out of: parentId, id.\n
-    Take as filter input a string of comma seperated parentIds/ids. Maximum is 50,even though max of maxResults=100.
-    Quota costs for 'part' paramter: id: 0, snippet: 1.\n
-    Idea for improvement: Calculate the most efficient way, if sippets should be retrieved by the get_comments_list() or get_comments_threads()'''
-    # Set false NextPageToken and empty output list
     commentThreadsNextPageToken = ''
     output = []
 
-    # Setup calculation for the costs for query
     costs = 0
     cost_per_query = 0
     if 'snippet' in part:
         cost_per_query += 2
 
-    # Loop as long as there are next pages of results
-
     while commentThreadsNextPageToken != None:
-        # Increment costs
         costs += cost_per_query
-        # Do the youtube request
         request = youtube.comments().list(
             part=part,
             maxResults=maxResults,
@@ -425,20 +359,17 @@ def get_comments_list(youtube, part="id", maxResults=100, parent_id=None, id=Non
         ).execute()
         response = request
 
-        # Append each element sepeerately to the output list
         [output.append(x) for x in response['items']]
-        # Get a NextPageToken in case there is one, else set it to None
         commentThreadsNextPageToken = response.get('nextPageToken')
 
-    # Print costs
     if costs > 0:
         logger.info(f'Comment list query costs: {costs}')
 
     return response
 
 def list_slice(input_list, n=50):
-    '''Concatenate n list elements separated by ',' and return a new list of concatenations.\n
-    Takes a list as input_list and an integer n for elements to concatenate.'''
+    # '''Concatenate n list elements separated by ',' and return a new list of concatenations.\n
+    # Takes a list as input_list and an integer n for elements to concatenate.'''
     s = 0
     e = n
     list_slices = []
@@ -449,7 +380,7 @@ def list_slice(input_list, n=50):
     return list_slices
 
 def comment_list_to_dict(reply_comments_snippets):
-    '''Return a dictionary. Take as input a .json file'''
+    # '''Return a dictionary. Take as input a .json file'''
     df_data = {
     'comment_id':[],
     'author_display_name':[],
@@ -486,80 +417,60 @@ def comment_list_to_dict(reply_comments_snippets):
 
 
 def get_all_comments(youtube, video_id):
-
     logger.info('Starting to get comment threads')
-    # Get list of snippets of threads
+
     thread_snippets = get_comment_threads(youtube, part="snippet,replies", video_id=video_id)
     logger.info('Done getting comment threads')
 
-    # Check if the thread comments have more than 5 replies and if true append the id to a dict
     thread_ids_with_more_replies = {}
     already_downloaded_replies = {}
 
-    # Loop through threads and search for >5 replies
     for t_id in thread_snippets:
         if t_id['snippet']['totalReplyCount'] > 5:
 
-            # Wite parent_id of threads with >5 replies to dict index
             thread_ids_with_more_replies[t_id['id']] = True
 
-            # This print statement tries to evaluate an error, that I couldn't fix yet
             logger.info(t_id['id'], 'has more than 5 replies')
             if t_id.get('replies') != None:
 
-                # Write reply id to dict
                 for reply_id in t_id['replies']['comments']:
                     already_downloaded_replies[reply_id['id']] = True
             else:
-                # This print statement tries to evaluate an error, that I couldn't fix yet
                 logger.info(f"* * * * * {t_id['id']} would throw a key error for 'replies' * * * * * ")
 
-    # Get a list of reply ids for thread ids with more than 5 replies
-    # Slice list into strings of 50 ids each
     strings_of_thread_ids_with_more_replies = list_slice(list(thread_ids_with_more_replies), n=50)
 
-    # Create empty dict for reply ids
     reply_ids = {}
 
-    # Loop through thread ids with >5 replies
     logger.info('Start getting reply comment ids, that were not downloaded yet')
     for id_string in thread_ids_with_more_replies:
-        # Get the reply ids
         replies = get_comments_list(youtube, part="id", parent_id=id_string, id=None, maxResults=100)
-        # Write the reply ids into a dict, if they are not downlaoded yet
         for r_id in replies['items']:
             if r_id['id'] not in already_downloaded_replies:
                 reply_ids[r_id['id']] = True
     logger.info('Done getting reply comment ids, that were not downloaded yet')
 
-    # Create a list with id strings
     strings_of_reply_ids = list_slice(list(reply_ids), n=50)
 
-    # Create empty list for new reply snippets
     reply_snippets = []
 
-    # Get reply snippets, that were not downloaded yet
     logger.info('Start downloading reply comments, that were not downloaded yet')
     for id_string in strings_of_reply_ids:
         r_snippets = get_comments_list(youtube, part="snippet", parent_id=None, id=id_string)
         reply_snippets += r_snippets['items']
     logger.info('Done downloading reply comments, that were not downloaded yet')
 
-    # Create an empty list for already downloaded replies (that came with threads)
     already_downloaded_replies = []
 
-    # Loop through threads and search for >0 replies
     for t_id in thread_snippets:
-        # Add already downloaded replies to list
         if t_id['snippet']['totalReplyCount'] > 0:
             already_downloaded_replies += t_id['replies']['comments']
 
-    # Add thread snippets + reply snippets together
     all_snippets = thread_snippets + reply_snippets + already_downloaded_replies
     return all_snippets
 
 def extract_comments(comments):
-    '''Extract comments from a json file.Return a dictionary. Take as input the result of function "get_all_comments()"'''
+    # '''Extract comments from a json file.Return a dictionary. Take as input the result of function "get_all_comments()"'''
     comment_dict = {}
     for c in comments:
         if c['kind'] == 'youtube#commentThread':
@@ -569,7 +480,7 @@ def extract_comments(comments):
     return comment_dict
 
 def concat_comments(comment_dict):
-    '''Concat comment in values of a dictionary and return one string. Used for WordCloud input data.'''
+    # '''Concat comment in values of a dictionary and return one string. Used for WordCloud input data.'''
     string = ''
     for i, c in comment_dict.items():
         result = re.findall('[a-z0-9]+', c.lower())
@@ -577,7 +488,7 @@ def concat_comments(comment_dict):
     return string
 
 def concat_listelements(series_object):
-    '''Concat elements in lists in series objects and return them as a string. Needed as input for a wordcloud.'''
+    # '''Concat elements in lists in series objects and return them as a string. Needed as input for a wordcloud.'''
     result = ''
     for tag_list in series_object:
         if tag_list:
@@ -586,7 +497,7 @@ def concat_listelements(series_object):
     return result
 
 def comments_to_df(all_comments):
-    '''Extract comments from "get_all_comments()" json and return a dataframe.'''
+    # '''Extract comments from "get_all_comments()" json and return a dataframe.'''
 
     new_dict = {
     'id':[],
@@ -614,7 +525,7 @@ def comments_to_df(all_comments):
     return comment_df
 
 def analyze_comment_sentiments(comment_df):
-    '''Analyse sentiment. Take as input a comment dataframe from "comments_to_df()"'''
+    # '''Analyse sentiment. Take as input a comment dataframe from "comments_to_df()"'''
     analyzer = SentimentIntensityAnalyzer()
 
     sentiment = {
@@ -634,38 +545,22 @@ def analyze_comment_sentiments(comment_df):
     return comment_sentiment
 
 def get_channel_video_df(youtube, channel_ids):
-    '''Get video data for a list of given channel ids and return a concatenated dataframe.'''
+    # '''Get video data for a list of given channel ids and return a concatenated dataframe.'''
 
     video_df = pd.DataFrame([])
     for channel_id in channel_ids:
 
         if video_df.empty == True:
-            # Get list of video ids
             v_list = videoIdList(youtube, channel_id)
-
-            # Get data for videos
             video_snippet_list = video_snippets(youtube, v_list, maxResults=50)
-
-            # Write data to a dict
             video_data_dict = snippets_to_dict(video_snippet_list, yt_credentials=youtube)
-
-            # Insert data into a dataframe
             video_df = pd.DataFrame(video_data_dict)
 
         else:
-            # Get list of video ids
             v_list = videoIdList(youtube, channel_id)
-
-            # Get data for videos
             video_snippet_list = video_snippets(youtube, v_list, maxResults=50)
-
-            # Write data to a dict
             video_data_dict = snippets_to_dict(video_snippet_list, yt_credentials=youtube)
-
-            # Insert data into a dataframe
             video_df_temp = pd.DataFrame(video_data_dict)
-
-            # Cancatenate dataframes
             video_df = pd.concat([video_df, video_df_temp])
 
     return video_df
